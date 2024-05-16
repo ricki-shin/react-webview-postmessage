2d8fd5cdb421a8dbd2ed2a2500f3907c6c11a056
ricki-shin <ricki@riduck.com>
2024-05-16 10:54:27 +0900

    Added graph for heart rate

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,49 +1,85 @@
-import { useEffect, useState } from "react";
-import styled from "styled-components";
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
 const Test = () => {
   const [allDevices, setAllDevices] = useState(null);
   const [connectedDevice, setConnectedDevice] = useState(null);
   const [bleData, setBleData] = useState(null);
+  const [heartRateList, setHeartRateList] = useState([]);
+
+  const startingPointX = 0;
+  const Dot = ({ x, y }) => {
+    // Calculate the position of the dot
+    const style = {
+      position: 'absolute',
+      left: x + 'px',
+      bottom: y + 'px',
+      width: '1px',
+      // Adjust size as needed
+      height: '1px',
+      // Adjust size as needed
+      backgroundColor: 'red',
+      // Adjust color as needed
+      borderRadius: '50%',
+      // To make it round
+    };
+    return <div style={style}></div>;
+  };
 
   //RN 으로부터 넘어오는 데이터 처리 로직
   useEffect(() => {
     const handleMessage = (e) => {
       const data = JSON.parse(e.data);
 
-      if (data.type === "allDevices") {
+      if (data.type === 'allDevices') {
         setAllDevices(data.data);
       }
 
-      if (data.type === "connectedDevice") {
+      if (data.type === 'connectedDevice') {
         setConnectedDevice(data.data);
       }
 
-      if (data.type === "bleData") {
+      if (data.type === 'bleData') {
+        console.log('incoming bleData from RN');
         setBleData(data.data);
+      }
+      if (data.type === 'heartRate') {
+        setBleData(data.data);
+        // WebView 에서 RN으로 데이터 수정해서 보냄
+        window.ReactNativeWebView.postMessage(
+          JSON.stringify({ type: data.type, data: data.data * 2 })
+        );
+        setHeartRateList((prevState) => {
+          if (prevState.length > 300) {
+            const updatedList = [...prevState.slice(1), data.data];
+            return updatedList;
+          } else {
+            return [...prevState, data.data];
+          }
+        });
       }
     };
 
     //ios
-    window.addEventListener("message", handleMessage);
+    window.addEventListener('message', handleMessage);
 
     //android
-    document.addEventListener("message", handleMessage);
+    document.addEventListener('message', handleMessage);
 
     return () => {
-      window.removeEventListener("message", handleMessage);
-      document.removeEventListener("message", handleMessage);
+      window.removeEventListener('message', handleMessage);
+      document.removeEventListener('message', handleMessage);
     };
   }, []);
 
   // WebView -> RN 으로 데이터 전달(기기 연결, 해제 를 해달라는 type값과 기기 id 전달)
   const hanldeClickWebToApp = (type, data) => {
-    if (type === "connect") {
+    if (type === 'connect') {
       window.ReactNativeWebView.postMessage(
         JSON.stringify({ type: type, data: data })
       );
     }
-    if (type === "disconnect") {
+    if (type === 'disconnect') {
       window.ReactNativeWebView.postMessage(
         JSON.stringify({ type: type, id: data })
       );
@@ -60,7 +96,7 @@ const Test = () => {
               <div>{el.id}</div>
               <div>{el.name}</div>
             </ColummBox>
-            <button onClick={() => hanldeClickWebToApp("connect", el)}>
+            <button onClick={() => hanldeClickWebToApp('connect', el)}>
               연결
             </button>
           </FlexBox>
@@ -77,15 +113,23 @@ const Test = () => {
                 <div>{el.id}</div>
                 <div>{el.name}</div>
               </ColummBox>
-              <button onClick={() => hanldeClickWebToApp("disconnect", el.id)}>
+              <button onClick={() => hanldeClickWebToApp('disconnect', el.id)}>
                 해제
               </button>
             </FlexBox>
           </>
         ))}
 
+      {bleData && <h3>bleData : {bleData}</h3>}
       {bleData && <h3>speedCadence : {bleData.speedCadence}</h3>}
       {bleData && <h3>power : {bleData.power}</h3>}
+      {/* {bleData && <h3>heart rate : {bleData}</h3>}
+      {bleData && <h3>heart rate list : {heartRateList}</h3>}
+      <GraphBox>
+        {heartRateList.map((heartRate, idx) => (
+          <Dot key={idx} x={startingPointX + idx} y={heartRate} />
+        ))}
+      </GraphBox> */}
     </Wrapper>
   );
 };
@@ -106,6 +150,12 @@ const FlexBox = styled.div`
 const ColummBox = styled.div`
   display: flex;
   flex-direction: column;
+`;
+const GraphBox = styled.div`
+  position: 'relative';
+  width: '300px';
+  height: '260px';
+  border: '1px solid black';
 `;
 
 const Title = styled.h1`
